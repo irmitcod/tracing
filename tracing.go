4afc62b2ea605410914c3fde8b1a95cf09c6f178
Behrooz Roustaei <behrooz.roustaei@gmail.com>
2022-06-11 13:57:46 +0430

    Add yeager

--- tracing.go
+++ tracing.go
@@ -1,11 +1,15 @@
 package tracing
 
 import (
+	"context"
 	"github.com/irmitcod/tracing/config"
+	"github.com/irmitcod/tracing/pkg/jaeger"
 	"github.com/irmitcod/tracing/pkg/log"
 	"github.com/irmitcod/tracing/pkg/metric"
+	"go.opentelemetry.io/otel"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"time"
 )
 
 type tracing struct {
@@ -13,7 +17,7 @@ type tracing struct {
 	metric.Metrics
 }
 
-func NewTracing(config config.Config) (*tracing, error) {
+func NewTracing(config *config.Config) (*tracing, error) {
 
 	metrics, err := metric.CreateMetrics(config.Metrics.URL, config.Metrics.ServiceName)
 	if err != nil {
@@ -25,6 +29,29 @@ func NewTracing(config config.Config) (*tracing, error) {
 	)
 	zapLogger := loggerZ.With(zap.String("service", "customer"))
 	factory := log.NewFactory(zapLogger)
+
+	tp, err := jaeger.InitJaeger(config)
+	if err != nil {
+		return nil, err
+	}
+
+	// Register our TracerProvider as the global so any imported
+	// instrumentation in the future will default to using it.
+	otel.SetTracerProvider(tp)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Cleanly shutdown and flush telemetry when the application exits.
+	defer func(ctx context.Context) {
+		// Do not make the application hang when it is shutdown.
+		ctx, cancel = context.WithTimeout(ctx, time.Second*5)
+		defer cancel()
+		if err := tp.Shutdown(ctx); err != nil {
+			zapLogger.Fatal(err.Error())
+		}
+	}(ctx)
+
 
 	return &tracing{Factory: factory, Metrics: metrics}, nil
 }
